a457b56362cfa65432dd35ff6f0d56fd82b6854f
kurokoo <80524293+lassksy@users.noreply.github.com>
2024-03-10 22:43:47 +0800

    push
    123

---
+++ src/components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+<header class="site-header">
+    <nav class="site-nav">
+      <a class="site-brand" href=" " title="home">
+        Woodude./Blog
+      </a>
+        <div class="site-links">
+        <a class="link">Home</a>
+        <a class="link">Blog</a>
+        <a class="link">Notes</a>
+        <a class="link">Rss</a>
+
+      </div>
+    </nav>
+
+  </header>
+
+</template>
+
+<script lang="ts" setup name="header" >
+    
+</script>
+
+<style scoped>
+.site-header {
+    padding-left: 25px;
+    padding-right: 25px;
+    font-size: 20px; 
+    margin:  25px auto; 
+  }
+
+  .site-brand {
+    font-weight: bold;
+    
+  }
+  .site-nav {
+    display: flex;
+  }
+  .site-links {
+    display: flex;
+    margin:  auto 0 0 auto;
+  }
+  .link {
+    margin-left: 20px;
+    padding: 5px;
+  }
+  .link:hover {
+    
+    background-color: hsl(0, 0%, 0%);
+    color: rgb(255, 255, 255);
+    transition: 0.4s;
+    border-radius: 0.25rem;
+    cursor: pointer;
+  }
+
+
+  .site-nav > a {
+    text-decoration: none;
+    color: rgb(0, 0, 0);
+    transition: 0.4s;
+    padding-left: 10px;
+}
+@media (prefers-color-scheme: dark) {
+  .site-nav > a {
+    text-decoration: none;
+    color: rgb(255, 255, 255);
+    transition: 0.4s;
+    padding-left: 10px;
+  }
+  .link:hover {
+    background-color: hsl(0, 0%, 100%);
+    color: rgb(0, 0, 0);
+    transition: 0.4s;
+    border-radius: 0.25rem;
+    cursor: pointer;
+    }
+}
+
+
+
+
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,161 @@
+<template>
+    <section class="project-main">
+        <div class="project-div">
+            <h1 class="h1-title">Project</h1>
+            <article class="project-text-main">
+                <header class="project-header">
+                    <h3 class="project-text-title">
+                        反正很厉害的app就是了
+                    </h3>
+                    <div class="project-links">
+                        <a  class="action-text project-link" href="">XXX.xx</a>
+                        <a class="action-text project-link" href="">github.com</a>   
+                        <a class="action-text project-link" href="">XXX.com</a>   
+
+                    </div>
+                </header>
+                    <section class="project-detil">
+                        <div>
+                            <p>这里将会展示我的各种mac软件(虽然现在都没有)
+                            </p>
+                            
+                            <p>这里将会展示我的各种mac软件(虽然现在都没有)
+                            </p>
+                            <p>这里将会展示我的各种mac软件(虽然现在都没有)
+                            </p>
+                        </div>
+                    </section>
+                    <div class="show-app">
+                            <img src="/src/assets/Lunar S.avif" alt="macapp">
+                    </div>
+            </article>
+
+
+            <article class="project-text-main">
+                <header class="project-header">
+                    <h3 class="project-text-title">
+                        反正很厉害的app就是了
+                    </h3>
+                    <div class="project-links">
+                        <a  class="action-text project-link" href="">XXX.xx</a>
+                        <a class="action-text project-link" href="">github.com</a>   
+                        <a class="action-text project-link" href="">XXX.com</a>   
+
+                    </div>
+                </header>
+                    <section class="project-detil">
+                        <div>
+                            <p>这里将会展示我的各种mac软件(虽然现在都没有)
+                            </p>
+                            
+                            <p>这里将会展示我的各种mac软件(虽然现在都没有)
+                            </p>
+                            <p>这里将会展示我的各种mac软件(虽然现在都没有)
+                            </p>
+                        </div>
+                    </section>
+                    <div class="show-app">
+                            <img src="/src/assets/Lunar S.avif" alt="macapp">
+                    </div>
+            </article>
+
+
+            <article class="project-text-main">
+                <header class="project-header">
+                    <h3 class="project-text-title">
+                        反正很厉害的app就是了
+                    </h3>
+                    <div class="project-links">
+                        <a  class="action-text project-link" href="">XXX.xx</a>
+                        <a class="action-text project-link" href="">github.com</a>   
+                        <a class="action-text project-link" href="">XXX.com</a>   
+
+                    </div>
+                </header>
+                    <section class="project-detil">
+                        <div>
+                            <p>这里将会展示我的各种mac软件(虽然现在都没有)
+                            </p>
+                            
+                            <p>这里将会展示我的各种mac软件(虽然现在都没有)
+                            </p>
+                            <p>这里将会展示我的各种mac软件(虽然现在都没有)
+                            </p>
+                        </div>
+                    </section>
+                    <div class="show-app">
+                            <img src="/src/assets/Lunar S.avif" alt="macapp">
+                    </div>
+            </article>
+            
+        </div>
+    </section>
+</template>
+
+<script lang="ts" setup name="project">
+
+</script>
+
+<style scoped>
+        .project-text-main {
+            padding-bottom: 30rem;
+        }
+
+        .project-main {
+            margin: auto auto;
+            margin-top:  8rem;
+        }
+        .project-div {
+            padding:  0 25px;
+        }
+
+        .project-header {
+            padding-bottom: 3rem;
+            border-bottom: solid 1px;
+        }
+
+        .project-text-title {
+            color: #005fc7;
+            font-weight: 900;
+            letter-spacing: 1em;
+            margin-top: 5rem;
+            margin-left: 1rem;
+            font-size: 2.5rem;
+            margin-bottom: 1rem;
+        }
+        .project-link {
+            margin-left: 20px;
+            font-size: 1.5rem;
+            line-height: 1.15rem;
+        }
+        .project-detil {
+            padding: 25px;
+            font-size: 1.3rem;
+        }
+        
+        p {
+          margin-bottom: 25px;  
+        }
+        
+        img {
+            position: relative;
+            height: 50%;
+            width: 50%;
+            top: -200px;
+            float: right;
+        }
+
+     
+
+        @media (prefers-color-scheme: dark) {
+            .project-text-title {
+            color: #005fc7;
+            font-weight: 900;
+            letter-spacing: 1em;
+            margin-top: 5rem;
+            margin-left: 1rem;
+            font-size: 2.5rem;
+            margin-bottom: 1rem;
+        }
+        }
+</style>
